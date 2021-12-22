254fa73dcd8f5282884a71d1e26c0a21c3e9db9b
Javier Armas Navarrete <javier.armasnav@gmail.com>
2021-12-22 18:41:40 -0500

    Added forms for create, update and delete blog posts

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse  # Reverse allows to reference an object by its URL template name
 
 # Create your models here.
 
@@ -9,6 +10,12 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
-    def __str__(self):
+    def __str__(self):  # You should add __str__() and get_absolute_url() method to each model you write.
         return self.title
+
+    def get_absolute_url(self):  # It sets a canonical URL for an object so even if the structure of your URLs changes,
+        # the reference to the specified object is the same.
+        return reverse('post_detail', args=[str(self.id)])  # Allows to reference post_detail. So we’re telling Django
+        # that the ultimate location of a Post entry is its post_detail view which is posts/<int:pk>/ (This means, after
+        # creating a post, the page will redirect you to the specific template of the post created).
 
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,9 +1,8 @@
-
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Post
 
 
 class BlogListView(ListView):
@@ -13,4 +12,24 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'post_detail.html'  # The name of the template that is on urls file
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'  # The name of the template that is on urls file
+    fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']  # We list the specific fields to be edited instead of using __all__ because we assume
+    # that the author of the post will be the same, so we just want to edit the title and the body.
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')  # reverse_lazy won't execute the URL redirect until the view has finished
+    # deleting the blog post
